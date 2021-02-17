f82bbf0d30ba497b1361ff9cc2e6b621d1e707f6
Vladimir <9264124625@mail.ru>
2021-02-17 12:12:40 +0300

    session-2

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "Адрес электронной почты.";
+            textBox1.ForeColor = Color.Gray;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath Button_Path = new System.Drawing.Drawing2D.GraphicsPath();
+            Button_Path.AddEllipse(0, 0, this.button1.Width, this.button1.Height);
+            Region Button_Region = new Region(Button_Path);
+            this.button1.Region = Button_Region;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            Graphics g = CreateGraphics();
+            // Создадим новый прямоугольник с размерами кнопки 
+            Rectangle smallRectangle = button1.ClientRectangle;
+            // уменьшим размеры прямоугольника 
+            smallRectangle.Inflate(-3, -3);
+            // создадим эллипс, используя полученные размеры 
+            gp.AddEllipse(smallRectangle);
+            button1.Region = new Region(gp);
+            // рисуем окантовоку для круглой кнопки 
+            g.DrawEllipse(new Pen(Color.Gray, 2),
+            button1.Left + 1,
+            button1.Top + 1,
+            button1.Width - 3,
+            button1.Height - 3);
+            // освобождаем ресурсы 
+            g.Dispose();
+        }
+    }
+}
